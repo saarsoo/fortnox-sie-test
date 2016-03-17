365fea3222474f3ea4a10f8c202bc62222c18eb8
Johan Saarsoo <johan.saarsoo@fortnox.se>
2016-03-17 16:04:41 +0100

    Initial commit.

---
+++ SIE Test/FormSIE.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace SIE_Test
+{
+    partial class FormSIETest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbSIEData = new System.Windows.Forms.RichTextBox();
+            this.bExport = new System.Windows.Forms.Button();
+            this.tbHost = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbAccessToken = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbClientSecret = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // rtbSIEData
+            // 
+            this.rtbSIEData.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbSIEData.Location = new System.Drawing.Point(12, 12);
+            this.rtbSIEData.Name = "rtbSIEData";
+            this.rtbSIEData.Size = new System.Drawing.Size(924, 372);
+            this.rtbSIEData.TabIndex = 0;
+            this.rtbSIEData.Text = "";
+            // 
+            // bExport
+            // 
+            this.bExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bExport.Location = new System.Drawing.Point(861, 468);
+            this.bExport.Name = "bExport";
+            this.bExport.Size = new System.Drawing.Size(75, 23);
+            this.bExport.TabIndex = 1;
+            this.bExport.Text = "Exportera";
+            this.bExport.UseVisualStyleBackColor = true;
+            this.bExport.Click += new System.EventHandler(this.bExport_Click);
+            // 
+            // tbHost
+            // 
+            this.tbHost.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbHost.Location = new System.Drawing.Point(94, 390);
+            this.tbHost.Name = "tbHost";
+            this.tbHost.Size = new System.Drawing.Size(842, 20);
+            this.tbHost.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 393);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Host";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 419);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Access-Token";
+            // 
+            // tbAccessToken
+            // 
+            this.tbAccessToken.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbAccessToken.Location = new System.Drawing.Point(94, 416);
+            this.tbAccessToken.Name = "tbAccessToken";
+            this.tbAccessToken.Size = new System.Drawing.Size(842, 20);
+            this.tbAccessToken.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 445);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Client-Secret";
+            // 
+            // tbClientSecret
+            // 
+            this.tbClientSecret.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbClientSecret.Location = new System.Drawing.Point(94, 442);
+            this.tbClientSecret.Name = "tbClientSecret";
+            this.tbClientSecret.Size = new System.Drawing.Size(842, 20);
+            this.tbClientSecret.TabIndex = 6;
+            // 
+            // FormSIETest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 503);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbClientSecret);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbAccessToken);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbHost);
+            this.Controls.Add(this.bExport);
+            this.Controls.Add(this.rtbSIEData);
+            this.Name = "FormSIETest";
+            this.Text = "SIE Test";
+            this.Load += new System.EventHandler(this.FormSIETest_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbSIEData;
+        private System.Windows.Forms.Button bExport;
+        private System.Windows.Forms.TextBox tbHost;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbAccessToken;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbClientSecret;
+    }
+}
+
---
+++ SIE Test/FormSIE.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FortnoxAPILibrary;
+using FortnoxAPILibrary.Connectors;
+
+namespace SIE_Test
+{
+    public partial class FormSIETest : Form
+    {
+        public FormSIETest()
+        {
+            InitializeComponent();
+        }
+
+        private void FormSIETest_Load(object sender, EventArgs e)
+        {
+            tbHost.Text = ConnectionCredentials.FortnoxAPIServer;
+        }
+
+        private void bExport_Click(object sender, EventArgs e)
+        {
+            ConnectionCredentials.FortnoxAPIServer = tbHost.Text;
+            ConnectionCredentials.AccessToken = tbAccessToken.Text;
+            ConnectionCredentials.ClientSecret = tbClientSecret.Text;
+
+            var connector = new SIEConnector();
+
+            try
+            {
+                connector.ExportSIE(SIEConnector.SIEType.SIE4, "test.sie");
+
+                if (connector.HasError)
+                {
+                    throw new Exception(connector.Error.Message);
+                }
+
+                using (var reader = new StreamReader("test.sie", Encoding.GetEncoding(437)))
+                {
+                    rtbSIEData.Text = reader.ReadToEnd();
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Misslyckades:\n\n" + exception.Message, "");
+            }
+        }
+    }
+}
